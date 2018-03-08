f850281e84285a528c9a63d2f6b0ef5c945a08ee
Shashank Srivatsa Kuchibhotla <sk.kuchibhotla@gmail.com>
2018-03-07 22:45:51 -0500

    Add project files.

---
+++ AutoShopCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutoShopCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
